7c03a9ae4b68d3761ad8e472c3991b6d2446362b
zaynab-nachabe <nachabezaynab5@gmail.com>
2025-07-09 11:20:59 +0200

    Added GUI and neural simulation implementation

---
+++ gui.py
@@ -0,0 +1,224 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+from matplotlib.widgets import TextBox, Slider, Button
+import random
+import traceback
+from simulation import run_simulation
+
+#graphic tool to observe parameter's affects on the network
+# let's start with the parameters : v_rest, v_tresh, tau_m, tau_refrac, tau_syn_E, tau_syn_I, cm, e_rev_E, e_rev_I
+# create an interactive plot, with input lines and the user can click to add spikes. 
+# have sliders for each parameter
+# the user will then move the slider and hit play to train the model with the new paramters
+# then we create the correspondant plot. 
+
+fig, ax = plt.subplots(figsize=(10, 8))
+plt.subplots_adjust(left=0.25, bottom=0.4, right=0.95, top=0.9)
+ax.set_title("Spikey", fontsize=16)
+
+current_num_inputs = 3
+current_duration = 100
+spike_lines = []
+input_spikes = []
+colors = []
+
+def setup_plot(num_inputs, duration):
+    global spike_lines, input_spikes, colors
+    
+    ax.clear()
+    
+    ax.set_xlim(0, duration)
+    ax.set_ylim(0, num_inputs + 0.5)
+    ax.set_xticks(np.arange(0, duration + 1, max(5, duration // 20)))
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('Input')
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
+    
+    ax.set_yticks(range(1, num_inputs + 1))
+    labels = [f"Input {i+1}" for i in range(num_inputs)]
+    ax.set_yticklabels(labels)
+    
+    ax.grid(True, which='major', linestyle='-')
+    ax.grid(True, which='minor', linestyle=':', alpha=0.5)
+    
+    colors = []
+    for i in range(num_inputs):
+        r = random.uniform(0.3, 0.9)
+        g = random.uniform(0.3, 0.9)
+        b = random.uniform(0.3, 0.9)
+        colors.append((r, g, b))
+    
+    spike_lines = [[] for _ in range(num_inputs)]
+    input_spikes = [[] for _ in range(num_inputs)]
+    
+    ax.set_title("little Spikey", fontsize=16)
+    
+    fig.canvas.draw_idle()
+
+ax_inputs = plt.axes([0.25, 0.95, 0.1, 0.03])
+text_box_inputs = TextBox(ax_inputs, 'Num Inputs: ', initial=str(current_num_inputs))
+
+ax_duration = plt.axes([0.55, 0.95, 0.1, 0.03])
+text_box_duration = TextBox(ax_duration, 'Duration (ms): ', initial=str(current_duration))
+
+ax_apply = plt.axes([0.75, 0.95, 0.1, 0.03])
+button_apply = Button(ax_apply, 'Apply')
+
+def apply_settings(event):
+    global current_num_inputs, current_duration
+    try:
+        num_inputs = int(text_box_inputs.text)
+        duration = int(text_box_duration.text)
+        
+        if num_inputs < 1:
+            print("Number of inputs must be at least 1")
+            return
+        if duration < 10:
+            print("Duration must be at least 10ms")
+            return
+            
+        current_num_inputs = num_inputs
+        current_duration = duration
+        
+        setup_plot(num_inputs, duration)
+        print(f"Plot updated with {num_inputs} inputs and {duration}ms duration")
+    except ValueError:
+        print("Please enter valid numbers")
+
+button_apply.on_clicked(apply_settings)
+
+def on_click(event):
+    if event.inaxes != ax:
+        return
+        
+    x = event.xdata
+    y = round(event.ydata)
+    
+    if 0 < y <= current_num_inputs and 0 <= x <= current_duration:
+        input_idx = y - 1
+        input_spikes[input_idx].append(x)
+        
+        line = ax.vlines(x=x, ymin=y-0.2, ymax=y+0.2, color=colors[input_idx], linewidth=2)
+        spike_lines[input_idx].append(line)
+        
+        fig.canvas.draw_idle()
+
+setup_plot(current_num_inputs, current_duration)
+fig.canvas.mpl_connect('button_press_event', on_click)
+
+slider_left = 0.25       
+slider_width = 0.65
+slider_height = 0.03
+slider_spacing = 0.04 
+bottom_margin = 0.05
+
+slider_positions = [bottom_margin + i*slider_spacing for i in range(7)]
+
+ax_cm = plt.axes([slider_left, slider_positions[0], slider_width, slider_height])
+ax_tau_syn_i = plt.axes([slider_left, slider_positions[1], slider_width, slider_height])
+ax_tau_syn_e = plt.axes([slider_left, slider_positions[2], slider_width, slider_height])
+ax_tau_refrac = plt.axes([slider_left, slider_positions[3], slider_width, slider_height])
+ax_tau_m = plt.axes([slider_left, slider_positions[4], slider_width, slider_height])
+ax_vthresh = plt.axes([slider_left, slider_positions[5], slider_width, slider_height])
+ax_vrest = plt.axes([slider_left, slider_positions[6], slider_width, slider_height])
+
+button_width = 0.15
+button_height = 0.04
+button_spacing = 0.05
+button_bottom = 0.01
+
+ax_reset_spikes = plt.axes([slider_left, button_bottom, button_width, button_height])
+ax_reset_params = plt.axes([slider_left + button_width + button_spacing, 
+                           button_bottom, button_width, button_height])
+ax_run = plt.axes([slider_left + 2*(button_width + button_spacing),
+                  button_bottom, button_width, button_height])
+slider_vrest = Slider(ax_vrest, 'v_rest (mV)', -80.0, -55.0, valinit=-65.0)
+slider_vthresh = Slider(ax_vthresh, 'v_thresh (mV)', -55.0, -40.0, valinit=-50.0)
+slider_tau_m = Slider(ax_tau_m, 'tau_m (ms)', 5.0, 50.0, valinit=20.0)
+slider_tau_refrac = Slider(ax_tau_refrac, 'tau_refrac (ms)', 0.1, 10.0, valinit=2.0)
+slider_tau_syn_e = Slider(ax_tau_syn_e, 'tau_syn_E (ms)', 1.0, 10.0, valinit=5.0)
+slider_tau_syn_i = Slider(ax_tau_syn_i, 'tau_syn_I (ms)', 1.0, 10.0, valinit=5.0)
+slider_cm = Slider(ax_cm, 'cm (nF)', 0.1, 2.0, valinit=1.0)
+
+button_reset_spikes = Button(ax_reset_spikes, 'Reset Spikes')
+button_reset_params = Button(ax_reset_params, 'Reset Params')
+button = Button(ax_run, 'Run')
+
+def update(val):
+    params = {
+        'v_rest': slider_vrest.val,
+        'v_thresh': slider_vthresh.val,
+        'tau_m': slider_tau_m.val,
+        'tau_refrac': slider_tau_refrac.val,
+        'tau_syn_E': slider_tau_syn_e.val,
+        'tau_syn_I': slider_tau_syn_i.val,
+        'cm': slider_cm.val
+    }
+    print(f"Updated parameters: {params}")
+    fig.canvas.draw_idle()
+
+def reset_spikes(event):
+    global spike_lines, input_spikes
+    
+    for input_idx in range(len(spike_lines)):
+        for line in spike_lines[input_idx]:
+            try:
+                line.remove()
+            except:
+                pass
+    
+    spike_lines = [[] for _ in range(current_num_inputs)]
+    input_spikes = [[] for _ in range(current_num_inputs)]
+    
+    fig.canvas.draw_idle()
+    print("Spikes reset")
+
+def reset_params(event):
+    slider_vrest.reset()
+    slider_vthresh.reset()
+    slider_tau_m.reset()
+    slider_tau_refrac.reset()
+    slider_tau_syn_e.reset()
+    slider_tau_syn_i.reset()
+    slider_cm.reset()
+    print("Parameters reset to default values.")
+
+def run_simulation_callback(event):
+    """Callback function for the 'Run' button that starts the simulation"""
+    try:
+        params = {
+            'v_rest': slider_vrest.val,
+            'v_thresh': slider_vthresh.val,
+            'tau_m': slider_tau_m.val,
+            'tau_refrac': slider_tau_refrac.val,
+            'tau_syn_E': slider_tau_syn_e.val,
+            'tau_syn_I': slider_tau_syn_i.val,
+            'cm': slider_cm.val
+        }
+        print(f"Running simulation with parameters: {params}")
+        for param, value in params.items():
+            print(f" {param}: {value}")
+        
+        #check for input spikes
+        if all(len(spikes) == 0 for spikes in input_spikes):
+            print("Warning: No input spikes detected. Please add some spikes by clicking on the plot.")
+            return
+        
+        run_simulation(params, input_spikes, colors, current_duration)
+        
+    except Exception as e:
+        print(f"Error in simulation: {str(e)}")
+        traceback.print_exc()
+
+slider_vrest.on_changed(update)
+slider_vthresh.on_changed(update)
+slider_tau_m.on_changed(update)
+slider_tau_refrac.on_changed(update)
+slider_tau_syn_e.on_changed(update)
+slider_tau_syn_i.on_changed(update)
+slider_cm.on_changed(update)
+button.on_clicked(run_simulation_callback)
+button_reset_spikes.on_clicked(reset_spikes)
+button_reset_params.on_clicked(reset_params)
+plt.show()
---
+++ simulation.py
@@ -0,0 +1,365 @@
+import pyNN.brian2 as sim
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+
+class NeuralSimulation:
+    def __init__(self):
+        self.sim = sim
+        self.excitatory_count = 5
+        self.inhibitory_count = 2
+        self.sim_duration = 0
+        self.populations = {}
+        self.projections = {}
+        self.spike_sources = []
+        self.input_to_excitatory_projections = []
+
+    def setup(self, params, input_spikes, duration):
+        """
+        Set up the neural simulation
+        
+        Parameters:
+        params (dict): Neuron parameters from the GUI sliders
+        input_spikes (list): List of spike times for each input
+        duration (float): Duration of the simulation in ms
+        """
+        self.sim_duration = duration + 50  #just a bit longer than the input duration
+        
+        #convert spike times to arrays
+        spike_arrays = []
+        for i, spikes in enumerate(input_spikes):
+            if spikes:  #check for spikes
+                spike_arrays.append(np.array(spikes))
+            else:
+                spike_arrays.append(np.array([]))
+        
+        if 'brian2' in self.sim.__name__.lower():
+            self.sim.setup(timestep=0.1, minimal_delay=0.1)
+        else:
+            self.sim.setup(timestep=0.1)
+        
+        cell_params = {
+            'v_rest': params['v_rest'],      # Resting membrane potential (mV)
+            'v_thresh': params['v_thresh'],  # Spike threshold (mV)
+            'tau_m': params['tau_m'],        # Membrane time constant (ms)
+            'tau_refrac': params['tau_refrac'],  # Refractory period (ms)
+            'tau_syn_E': params['tau_syn_E'],  # Excitatory synapse time constant (ms)
+            'tau_syn_I': params['tau_syn_I'],  # Inhibitory synapse time constant (ms)
+            'cm': params['cm'],              # Membrane capacitance (nF)
+            'i_offset': 0.0,                 # Offset current (nA)
+            'v_reset': params['v_rest'] + 5.0  # Reset potential after spike (mV)
+        }
+        
+        #create input spike sources
+        self.spike_sources = []
+        for i, spike_array in enumerate(spike_arrays):
+            if len(spike_array) > 0:
+                spike_source = self.sim.SpikeSourceArray(spike_times=spike_array.tolist())
+                self.spike_sources.append(self.sim.Population(1, spike_source, label=f"input_{i}"))
+            else:
+                #if there's no spikes for this input still create a source
+                spike_source = self.sim.SpikeSourceArray(spike_times=[])
+                self.spike_sources.append(self.sim.Population(1, spike_source, label=f"input_{i}"))
+        
+        if hasattr(self.sim, 'IF_cond_exp'):
+            neuron_model = self.sim.IF_cond_exp
+        else:
+            #if the conductance-based model isn't available
+            neuron_model = self.sim.IF_curr_exp
+            
+        self.populations['excitatory'] = self.sim.Population(
+            self.excitatory_count, 
+            neuron_model(**cell_params),
+            label="Excitatory"
+        )
+        
+        self.populations['inhibitory'] = self.sim.Population(
+            self.inhibitory_count,
+            neuron_model(**cell_params),
+            label="Inhibitory"
+        )
+        
+        self.populations['excitatory'].initialize(v=params['v_rest'] + np.random.uniform(0, 10, size=self.excitatory_count))
+        self.populations['inhibitory'].initialize(v=params['v_rest'] + np.random.uniform(0, 10, size=self.inhibitory_count))
+        
+        stdp_model = self._create_stdp_model()
+        
+        self.input_to_excitatory_projections = []
+        for i, source in enumerate(self.spike_sources):
+            if stdp_model:
+                projection = self.sim.Projection(
+                    source, self.populations['excitatory'],
+                    connector=self.sim.AllToAllConnector(),
+                    synapse_type=stdp_model,
+                    receptor_type="excitatory",
+                    label=f"Input{i} to Excitatory"
+                )
+            else:
+                #static synapse if STDP isn't available
+                projection = self.sim.Projection(
+                    source, self.populations['excitatory'],
+                    connector=self.sim.AllToAllConnector(),
+                    synapse_type=self.sim.StaticSynapse(weight=0.2, delay=1.0),
+                    receptor_type="excitatory",
+                    label=f"Input{i} to Excitatory"
+                )
+            self.input_to_excitatory_projections.append(projection)
+        
+        #connect excitatory to inhibitory
+        self.projections['e_to_i'] = self.sim.Projection(
+            self.populations['excitatory'], self.populations['inhibitory'],
+            connector=self.sim.AllToAllConnector(),
+            synapse_type=self.sim.StaticSynapse(weight=0.3, delay=1.0),
+            receptor_type="excitatory",
+            label="E to I"
+        )
+        
+        #connect inhibitory to excitatory (feedback inhibition)
+        self.projections['i_to_e'] = self.sim.Projection(
+            self.populations['inhibitory'], self.populations['excitatory'],
+            connector=self.sim.AllToAllConnector(),
+            synapse_type=self.sim.StaticSynapse(weight=0.2, delay=1.0),
+            receptor_type="inhibitory",
+            label="I to E"
+        )
+        
+        self.populations['excitatory'].record(['spikes', 'v'])
+        self.populations['inhibitory'].record(['spikes', 'v'])
+
+    def _create_stdp_model(self):
+        """Create STDP model based on the backend being used"""
+        tau_plus = 20.0  # ms
+        tau_minus = 20.0  # ms
+        w_max = 0.5      # maximum weight
+        A_plus = 0.01    # amplitude of potentiation
+        A_minus = 0.0105 # amplitude of depression
+        
+        try:
+            backend_name = self.sim.__name__
+            
+            if 'brian2' in backend_name.lower():
+                print("Using Brian2 backend STDP implementation")
+                print("Brian2 backend detected - using static synapses instead of STDP")
+                return None
+            elif 'nest' in backend_name.lower():
+                print("Using NEST backend STDP implementation")
+                #NEST backend has A_plus and A_minus
+                return self.sim.STDPMechanism(
+                    timing_dependence=self.sim.SpikePairRule(tau_plus=tau_plus, tau_minus=tau_minus),
+                    weight_dependence=self.sim.AdditiveWeightDependence(w_min=0, w_max=w_max, A_plus=A_plus, A_minus=A_minus),
+                    weight=0.2, delay=1.0
+                )
+            else:
+                print(f"Using generic STDP implementation for {backend_name} backend")
+                # Try simpler constructor without the A_plus/A_minus parameters
+                return self.sim.STDPMechanism(
+                    timing_dependence=self.sim.SpikePairRule(tau_plus=tau_plus, tau_minus=tau_minus),
+                    weight_dependence=self.sim.AdditiveWeightDependence(w_min=0, w_max=w_max),
+                    weight=0.2, delay=1.0
+                )
+        except Exception as e:
+            print(f"Warning: STDP setup failed: {str(e)}")
+            print("Using static synapses instead")
+            return None
+    
+    def run(self):
+        """Run the simulation for the specified duration"""
+        print(f"Running simulation for {self.sim_duration}ms...")
+        self.sim.run(self.sim_duration)
+        
+    def get_data(self):
+        """Retrieve simulation results"""
+        print("Retrieving simulation results...")
+        excitatory_data = self.populations['excitatory'].get_data()
+        inhibitory_data = self.populations['inhibitory'].get_data()
+        return excitatory_data, inhibitory_data
+    
+    def plot_results(self, params, input_spikes, colors):
+        """Plot the simulation results"""
+        excitatory_data, inhibitory_data = self.get_data()
+        
+        result_fig = plt.figure(figsize=(12, 10))
+        gs = plt.GridSpec(4, 1, height_ratios=[1, 1, 1, 2])
+        
+        ax_inputs = plt.subplot(gs[0])
+        for i, spikes in enumerate(input_spikes):
+            if spikes:
+                ax_inputs.scatter(spikes, [i+1] * len(spikes), marker='|', s=100, color=colors[i], label=f"Input {i+1}")
+        ax_inputs.set_title("Input Spike Trains")
+        ax_inputs.set_xlabel("Time (ms)")
+        ax_inputs.set_ylabel("Input")
+        ax_inputs.set_yticks(range(1, len(input_spikes)+1))
+        ax_inputs.set_xlim(0, self.sim_duration)
+        ax_inputs.grid(True)
+        
+        # Plot excitatory neuron spikes
+        ax_exc = plt.subplot(gs[1])
+        for segment in excitatory_data.segments:
+            if segment.spiketrains:
+                for i, spiketrain in enumerate(segment.spiketrains):
+                    if len(spiketrain) > 0:
+                        spike_times = spiketrain.times.magnitude
+                        ax_exc.scatter(spike_times, [i+1] * len(spike_times), marker='|', s=100, color='r')
+        ax_exc.set_title("Excitatory Neuron Spikes")
+        ax_exc.set_xlabel("Time (ms)")
+        ax_exc.set_ylabel("Neuron Index")
+        ax_exc.set_yticks(range(1, self.excitatory_count+1))
+        ax_exc.set_xlim(0, self.sim_duration)
+        ax_exc.grid(True)
+        
+        ax_inh = plt.subplot(gs[2])
+        for segment in inhibitory_data.segments:
+            if segment.spiketrains:
+                for i, spiketrain in enumerate(segment.spiketrains):
+                    if len(spiketrain) > 0:
+                        spike_times = spiketrain.times.magnitude
+                        ax_inh.scatter(spike_times, [i+1] * len(spike_times), marker='|', s=100, color='b')
+        ax_inh.set_title("Inhibitory Neuron Spikes")
+        ax_inh.set_xlabel("Time (ms)")
+        ax_inh.set_ylabel("Neuron Index")
+        ax_inh.set_yticks(range(1, self.inhibitory_count+1))
+        ax_inh.set_xlim(0, self.sim_duration)
+        ax_inh.grid(True)
+        
+        #membrane potentials
+        ax_v = plt.subplot(gs[3])
+        plot_colors = plt.cm.viridis(np.linspace(0, 1, self.excitatory_count))
+        
+        #membrane potential of excitatory neurons
+        for segment in excitatory_data.segments:
+            if hasattr(segment, 'analogsignals') and len(segment.analogsignals) > 0:
+                vm = segment.analogsignals[0]
+                for i in range(self.excitatory_count):
+                    ax_v.plot(vm.times.magnitude, vm[:, i].magnitude, 
+                             color=plot_colors[i], label=f'E{i+1}')
+        
+        #threshold line
+        ax_v.axhline(y=params['v_thresh'], color='r', linestyle='--', label='Threshold')
+        #rest potential line
+        ax_v.axhline(y=params['v_rest'], color='k', linestyle=':', label='Rest')
+        
+        ax_v.set_title("Excitatory Neuron Membrane Potentials")
+        ax_v.set_xlabel("Time (ms)")
+        ax_v.set_ylabel("Membrane Potential (mV)")
+        ax_v.set_xlim(0, self.sim_duration)
+        ax_v.grid(True)
+        ax_v.legend(loc='upper right', ncol=2)
+        
+        param_text = f"Parameters: v_rest={params['v_rest']:.1f}mV, v_thresh={params['v_thresh']:.1f}mV, τm={params['tau_m']:.1f}ms, τrefrac={params['tau_refrac']:.1f}ms"
+        result_fig.suptitle(param_text, fontsize=10)
+        
+        plt.tight_layout()
+        
+        self.plot_weights(input_spikes)
+        
+    def plot_weights(self, input_spikes):
+        """Plot the synaptic weights after learning"""
+        # Show weights after learning
+        print("Synaptic weights after learning:")
+        all_weights = []
+        weight_labels = []
+        
+        # The get method has different signatures in different backends
+        for i, proj in enumerate(self.input_to_excitatory_projections):
+            try:
+                # Try different approaches based on the backend
+                try:
+                    # For some backends, like NEST
+                    weights = proj.get('weight')
+                except TypeError:
+                    try:
+                        # For Brian2 backend
+                        weights = proj.get('weight', format='list')
+                    except:
+                        # Try with array format
+                        weights = proj.get('weight', format='array')
+                
+                print(f"Input {i+1} to Excitatory weights: {weights}")
+                
+                # Store weights for plotting if this input had spikes
+                if len(input_spikes[i]) > 0:
+                    if weights is not None and len(weights) > 0:
+                        # Convert to flat numpy array for consistent handling
+                        if isinstance(weights, list):
+                            if isinstance(weights[0], tuple):  # List of (source, target, weight) tuples
+                                w_array = np.array([w for _, _, w in weights])
+                            else:
+                                w_array = np.array(weights)
+                        else:
+                            w_array = np.array(weights).flatten()
+                        
+                        all_weights.append(w_array)
+                        weight_labels.append(f"Input {i+1}")
+            except Exception as e:
+                print(f"Couldn't retrieve weights for Input {i+1}: {str(e)}")
+        
+        # Create a weights visualization figure
+        try:
+            if all_weights:  # Only create plot if we have weights to show
+                # Use the simplest approach
+                plt.figure("Synaptic Weights")  # Name the figure for easy identification
+                plt.boxplot(all_weights, labels=weight_labels)
+                plt.title("Synaptic Weights after Learning")
+                plt.xlabel("Input")
+                plt.ylabel("Weight")
+                plt.grid(True)
+                
+                # This will display the figure
+                plt.show(block=False)
+                
+                print("Weight visualization created successfully")
+            else:
+                print("No weights available to visualize")
+            
+        except Exception as e:
+            print(f"Error creating weight visualization: {str(e)}")
+            
+    def end(self):
+        """Clean up the simulation"""
+        try:
+            self.sim.end()
+        except:
+            pass
+        print("Simulation complete.")
+
+# Function to run a complete simulation
+def run_simulation(params, input_spikes, colors, duration):
+    """
+    Run a full neural network simulation
+    
+    Parameters:
+    params (dict): Dictionary of neuron parameters
+    input_spikes (list): List of spike times for each input
+    colors (list): List of colors for visualization
+    duration (float): Duration of the simulation in ms
+    
+    Returns:
+    bool: Whether the simulation was successful
+    """
+    try:
+        # Create simulation object
+        simulation = NeuralSimulation()
+        
+        # Setup simulation
+        simulation.setup(params, input_spikes, duration)
+        
+        # Run simulation
+        simulation.run()
+        
+        # Plot results
+        simulation.plot_results(params, input_spikes, colors)
+        
+        # End simulation
+        simulation.end()
+        
+        return True
+    except Exception as e:
+        print(f"Error in simulation: {str(e)}")
+        import traceback
+        traceback.print_exc()
+        try:
+            # Try to clean up
+            simulation.end()
+        except:
+            pass
+        return False
